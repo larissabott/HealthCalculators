d5efd6a16852ca73266389c82e02d5e37e647b55
Lala Bott <lala@Lalas-MacBook-Air.local>
2021-05-30 13:44:44 -0500

    bmi calc upgrade

--- BMIcalculator.js
+++ BMIcalculator.js
@@ -8,13 +8,13 @@ function bmiCalculator (weight, height) //uses metric system: weight in kilogram
     if (bmi > 18.6 && bmi <= 24.9) {
        return("Your BMI is " + bmi + ", so you have a normal weight.");
     }
-    if (bmi > 25 && bmi <= 29.9) {
+    if (bmi > 24.9 && bmi <= 29.9) {
        return("Your BMI is " + bmi + ", so you are overweight.");
     }
-    if (bmi > 30 && bmi <= 34.9) {
+    if (bmi > 29.9 && bmi <= 34.9) {
        return("Your BMI is " + bmi + ", so you are obese.");
     }
-    if (bmi >35) {
+    if (bmi > 34.9) {
        return("Your BMI is " + bmi + ", so you are extremely obese.");
     }
 } bmiCalculator(weight, height);
