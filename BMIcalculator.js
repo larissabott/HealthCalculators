19199b94b6d27db40b8d722ddb40ec0c0d5a72ab
Lala Bott <lala@Lalas-MacBook-Air.local>
2021-05-30 13:39:11 -0500

    bmi calc update

--- BMIcalculator.js
+++ BMIcalculator.js
@@ -1,6 +1,20 @@
-function bmiCalculator(weight, height) {
-    var bmi = weight / (height * height);
-    return Math.round(bmi);
-}
-var bmi = bmiCalculator(weight, height);
-consol.log(bmi); //weight in kilograms, height in meters
+function bmiCalculator (weight, height) //uses metric system: weight in kilograms & height in meters
+{
+   var bmi = Math.round(weight / (height*height));
+
+    if (bmi < 18.5) {
+        return("Your BMI is " + bmi + ", so you are underweight.");
+    }
+    if (bmi > 18.6 && bmi <= 24.9) {
+       return("Your BMI is " + bmi + ", so you have a normal weight.");
+    }
+    if (bmi > 25 && bmi <= 29.9) {
+       return("Your BMI is " + bmi + ", so you are overweight.");
+    }
+    if (bmi > 30 && bmi <= 34.9) {
+       return("Your BMI is " + bmi + ", so you are obese.");
+    }
+    if (bmi >35) {
+       return("Your BMI is " + bmi + ", so you are extremely obese.");
+    }
+} bmiCalculator(weight, height);
